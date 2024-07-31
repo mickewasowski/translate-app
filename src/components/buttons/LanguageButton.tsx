b010de905187d38d87ca02eba448597077cdbf87
Rosen Yorgov <yorgov.h.r@gmail.com>
2024-07-31 13:30:41 +0300

    implemented languages dropdown menu

--- src/components/buttons/LanguageButton.tsx
+++ src/components/buttons/LanguageButton.tsx
@@ -1,23 +1,69 @@
 import classNames from "classnames";
 import './LanguageButton.styles.scss';
+import { ReactSVG } from "react-svg";
+import { useState } from "react";
+import { allLanguages } from "../../contexts/UserInput";
 
 interface IProps {
     innerText: string;
     currentlySelected: boolean;
     setLanguage: () => void;
     id: string;
+    hasDropdown: boolean;
 }
 
-function LanguageButton({ innerText, currentlySelected, setLanguage, id }: IProps) {
+const dropdownArrow = '/src/assets/Expand_down.svg';
+
+function LanguageButton({ innerText, currentlySelected, setLanguage, id, hasDropdown }: IProps) {
+    const [expanded, setExpanded] = useState(false);
+    const dropDownButtons = allLanguages.slice(3);
+
     const className = classNames({
         'active-button': currentlySelected,
         'regular': true,
     });
 
+
+    const handleDropDownClick = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setExpanded(!expanded);
+    }
+
+    const handleSelectLanguage = (event, btnId: string) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setLanguage(btnId);
+        setExpanded(!expanded);
+    }
+
     return(
-        <button className={className} onClick={() => setLanguage(id)}>
-            {innerText}
-        </button>
+        <div className="language-btn-wrapper">
+            <button className={className} onClick={() => setLanguage(id)}>
+                { innerText }
+                { hasDropdown &&
+                    (
+                        <span id="dropdown-btn" onClick={handleDropDownClick}>
+                            <ReactSVG src={dropdownArrow}/>
+                        </span>
+                    )
+                }
+            </button>
+            {
+                hasDropdown && expanded &&
+                (
+                    <div className="dropdown-menu">
+                        <ul>
+                            {
+                                dropDownButtons.map((btn) => {
+                                    return <li key={btn.id} onClick={(event) => handleSelectLanguage(event, btn.id)}>{btn.innerText}</li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
